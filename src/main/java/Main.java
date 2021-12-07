0a646bd0a62790953bcd433c691d7816521c6e20
OMER\Omer <omer8882@gmail.com>
2021-12-07 04:00:45 +0200

    no message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 //        String pdfAbsolutePath = "//Users/nivdan//Downloads//distri//sample.pdf";
 //        PDFConverter converter = new PDFConverter();
-            Worker.main(args);
+           // Worker.main(args);
 //        short action = PDFConverter.ToImage | PDFConverter.ToHTML | PDFConverter.ToText;
 //        converter.convert(action, pdfAbsolutePath);
-       // String pdfAbsolutePath = "C:\\Users\\Omer\\Omer\\University\\Year 3\\Semester 5\\Distributed\\Assignment1\\src\\main\\java\\small-input.txt";
-        //LocalApplication localApplication =  new LocalApplication();
-        //localApplication.run(pdfAbsolutePath ,10, true);
+        String pdfAbsolutePath = "C:\\Users\\Omer\\Omer\\University\\Year 3\\Semester 5\\Distributed\\Assignment1\\src\\main\\java\\small-input.txt";
+        LocalApplication localApplication =  new LocalApplication();
+        localApplication.run(pdfAbsolutePath ,10, true);
 //        Region region = Region.US_EAST_1;
 //        Ec2Client  ec2Client = Ec2Client.builder().region(region).build();
 
